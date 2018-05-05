cac2ca624960b8b742441ec2e6ab00574c157b71
marwens11 <36227464+marwens11@users.noreply.github.com>
2018-05-05 22:48:34 +0100

    Merge branch 'master' of https://github.com/RansomGr/MySoulMate-Mobile-
    # Conflicts:
    #	src/Views/hableu.java

---
+++ src/Views/Plan/DetailPlanView.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Views.Plan;
+
+/**
+ * GUI builder created Form
+ *
+ * @author irou
+ */
+public class DetailPlanView extends com.codename1.ui.Form {
+
+    public DetailPlanView() {
+        this(com.codename1.ui.util.Resources.getGlobalResources());
+    }
+    
+    public DetailPlanView(com.codename1.ui.util.Resources resourceObjectInstance) {
+        initGuiBuilderComponents(resourceObjectInstance);
+    }
+
+//-- DON'T EDIT BELOW THIS LINE!!!
+
+
+// <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initGuiBuilderComponents(com.codename1.ui.util.Resources resourceObjectInstance) {
+        setLayout(new com.codename1.ui.layouts.LayeredLayout());
+        setInlineStylesTheme(resourceObjectInstance);
+                setInlineStylesTheme(resourceObjectInstance);
+        setTitle("DetailPlanView");
+        setName("DetailPlanView");
+    }// </editor-fold>
+
+//-- DON'T EDIT ABOVE THIS LINE!!!
+}
