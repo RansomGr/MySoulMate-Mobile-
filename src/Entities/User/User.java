e58293f680200cd5b440cb3a9802c712f55752ad
nanofarade <nadia.trabelsi@esprit.tn>
2018-05-06 15:42:47 +0100

    Merge branch 'master' of https://github.com/RansomGr/MySoulMate-Mobile-
    # Conflicts:
    #	src/Views/Matching/UtilisateurView.java

--- src/Entities/User/User.java
+++ src/Entities/User/User.java
@@ -5,6 +5,7 @@
  */
 package Entities.User;
 
+import Entities.Profil.Profil;
 import java.util.Date;
 
 /**
@@ -27,10 +28,11 @@ public class User {
     private String last_login;
     private String confirmation_token;
     private String role;
-    private int adresse;
+    private Adresse adresse;
     private int matchtot;
+    private Profil profil;
 
-    public User(int id, String nom, String prenom, String gender, String username, String username_canonical, String email, String email_canonical, int enabled, String salt, String password, String last_login, String confirmation_token, String role,int adresse) {
+    public User(int id, String nom, String prenom, String gender, String username, String username_canonical, String email, String email_canonical, int enabled, String salt, String password, String last_login, String confirmation_token, String role,Adresse adresse) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
@@ -164,11 +166,11 @@ public class User {
         this.role = role;
     }
     
-        public int getAdresse() {
+        public Adresse getAdresse() {
         return adresse;
     }
 
-    public void setAdresse(int adresse) {
+    public void setAdresse(Adresse adresse) {
         this.adresse = adresse;
     }
     
@@ -189,13 +191,21 @@ public class User {
         this.datenaissance = datenaissance;
     }
     
+        public Profil getProfil() {
+        return profil;
+    }
+
+    public void setProfil(Profil profil) {
+        this.profil = profil;
+    }
+    
 
     @Override
     public String toString() {
         return "User{" + "id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", gender=" + gender + ", username=" + username + ", username_canonical=" + username_canonical + ", email=" + email + ", email_canonical=" + email_canonical + ", enabled=" + enabled + ", salt=" + salt + ", password=" + password + ", last_login=" + last_login + ", confirmation_token=" + confirmation_token + ", role=" + role + '}';
     }
 
-    public User(String nom, String prenom, String gender, String username, String username_canonical, String email, String email_canonical, int enabled, String salt, String password, String last_login, String confirmation_token, String role, int adresse) {
+    public User(String nom, String prenom, String gender, String username, String username_canonical, String email, String email_canonical, int enabled, String salt, String password, String last_login, String confirmation_token, String role, Adresse adresse) {
         this.nom = nom;
         this.prenom = prenom;
         this.gender = gender;
